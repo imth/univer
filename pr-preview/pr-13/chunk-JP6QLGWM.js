035c25366e570d432e10340f926b1e369ea51920
imth <imth@users.noreply.github.com>
2026-05-07 05:55:02 +0000

    Remove preview for PR  🛬

--- pr-preview/pr-13/chunk-JP6QLGWM.js
+++
@@ -1,7 +0,0 @@
-// ../packages/sheets-formula-ui/src/locale/function-list/univer/en-US.ts
-var locale = {};
-var en_US_default = locale;
-
-export {
-  en_US_default
-};
--- pr-preview/pr-13/zh-CN-GTE3V5UW.js
+++
@@ -1,8 +0,0 @@
-import {
-  zh_CN_default
-} from "./chunk-CDZYV7BA.js";
-import "./chunk-RXFCYNLY.js";
-import "./chunk-24OICD5T.js";
-export {
-  zh_CN_default as default
-};
