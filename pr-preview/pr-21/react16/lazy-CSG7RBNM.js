2071cd09a5cbd4b701ee40bb9676e1b803b8f9a2
imth <imth@users.noreply.github.com>
2026-05-13 10:33:04 +0000

    Deploy preview for PR  🛫

---
+++ pr-preview/pr-21/react16/lazy-CSG7RBNM.js
@@ -0,0 +1,67 @@
+import {
+  UniverSheetsThreadCommentUIPlugin
+} from "./chunk-DX3BXA2O.js";
+import {
+  UniverSheetsNoteUIPlugin,
+  UniverSheetsTableUIPlugin
+} from "./chunk-QM5KF6KX.js";
+import {
+  UniverDocsMentionUIPlugin
+} from "./chunk-XSYLA4MU.js";
+import {
+  UniverThreadCommentUIPlugin
+} from "./chunk-GQG566ZE.js";
+import "./chunk-OEDWNGYU.js";
+import "./chunk-TYXMO7FW.js";
+import "./chunk-5SIMKMJF.js";
+import {
+  UniverDocsDrawingUIPlugin
+} from "./chunk-HJXP54CP.js";
+import "./chunk-TM5QNBBA.js";
+import {
+  UniverSheetsDrawingUIPlugin
+} from "./chunk-XSI67AV7.js";
+import {
+  UniverSheetsConditionalFormattingUIPlugin,
+  UniverSheetsDataValidationUIPlugin,
+  UniverSheetsFilterUIPlugin
+} from "./chunk-CKSTTFUW.js";
+import "./chunk-JD5HTIDB.js";
+import "./chunk-CUW7VLLI.js";
+import {
+  UniverSheetsNumfmtUIPlugin
+} from "./chunk-67DUF45A.js";
+import {
+  UniverSheetsFormulaUIPlugin
+} from "./chunk-ENGOP3NU.js";
+import "./chunk-YQYBDCF5.js";
+import "./chunk-LKVXGXDI.js";
+import "./chunk-XHQE667S.js";
+import "./chunk-6NLCZNFG.js";
+import "./chunk-LJNGJSAA.js";
+import "./chunk-OZJJTPRS.js";
+import "./chunk-53LCI556.js";
+import "./chunk-MR7DLPM2.js";
+import "./chunk-EQ2B2W73.js";
+import "./chunk-24OICD5T.js";
+
+// src/sheets-no-worker/lazy.ts
+function getLazyPlugins() {
+  return [
+    [UniverDocsDrawingUIPlugin],
+    [UniverDocsMentionUIPlugin],
+    [UniverSheetsNumfmtUIPlugin],
+    [UniverThreadCommentUIPlugin],
+    [UniverSheetsThreadCommentUIPlugin],
+    [UniverSheetsNoteUIPlugin],
+    [UniverSheetsTableUIPlugin],
+    [UniverSheetsFormulaUIPlugin],
+    [UniverSheetsDataValidationUIPlugin],
+    [UniverSheetsConditionalFormattingUIPlugin],
+    [UniverSheetsFilterUIPlugin, { useRemoteFilterValuesGenerator: false }],
+    [UniverSheetsDrawingUIPlugin]
+  ];
+}
+export {
+  getLazyPlugins as default
+};
---
+++ pr-preview/pr-21/react16/zh-CN-FCGHIWUT.js
@@ -0,0 +1,8 @@
+import {
+  zh_CN_default
+} from "./chunk-YRBPDJQT.js";
+import "./chunk-MR7DLPM2.js";
+import "./chunk-24OICD5T.js";
+export {
+  zh_CN_default as default
+};
