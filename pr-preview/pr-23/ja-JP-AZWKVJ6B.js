1fe7932b8eb9d85d9da6f9b5bf0721f009c62a93
imth <imth@users.noreply.github.com>
2026-05-14 06:30:29 +0000

    Deploy preview for PR  🛫

---
+++ pr-preview/pr-23/ja-JP-AZWKVJ6B.js
@@ -0,0 +1,9 @@
+import {
+  ja_JP_default
+} from "./chunk-XXHGEJYF.js";
+import "./chunk-HEVHAOJL.js";
+import "./chunk-MR7DLPM2.js";
+import "./chunk-24OICD5T.js";
+export {
+  ja_JP_default as default
+};
